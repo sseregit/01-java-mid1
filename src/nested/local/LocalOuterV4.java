7aa057c19cebc6cb6508aba0cd875abed459e631
장연식 <sseregit@knowwheresoft.com>
2024-11-26 11:56:16 +0900

    지역 클래스 - 지역 변수 캡처3

---
+++ src/nested/local/LocalOuterV4.java
@@ -0,0 +1,42 @@
+package nested.local;
+
+import java.lang.reflect.Field;
+
+public class LocalOuterV4 {
+
+    private int outInstanceVar = 3;
+
+    public Printer process(int paramVar) {
+        int localVar = 1;
+
+        class LocalPrinter implements Printer {
+            int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value = " + value);
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+
+        LocalPrinter printer = new LocalPrinter();
+//        localVar = 10;
+//        paramVar = 10;
+        return printer;
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV4 localOuter = new LocalOuterV4();
+        Printer printer = localOuter.process(2);
+        printer.print();
+
+        System.out.println("필드 확인");
+
+        Field[] fields = printer.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field);
+        }
+    }
+}
