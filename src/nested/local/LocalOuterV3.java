31fecc0b87ade3f02bb6b2adb0a1f7ca3f352ee7
장연식 <sseregit@knowwheresoft.com>
2024-11-26 11:24:40 +0900

    지역 클래스 - 지역 변수 캡처1

---
+++ src/nested/local/LocalOuterV3.java
@@ -0,0 +1,32 @@
+package nested.local;
+
+public class LocalOuterV3 {
+
+    private int outInstanceVar = 3;
+
+    public Printer process(int paramVar) {
+        int localVar = 1;
+
+        class LocalPrinter implements Printer {
+            int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value = " + value);
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+
+        LocalPrinter printer = new LocalPrinter();
+//        printer.print();
+        return printer;
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV3 localOuter = new LocalOuterV3();
+        Printer printer = localOuter.process(2);
+        printer.print();
+    }
+}
