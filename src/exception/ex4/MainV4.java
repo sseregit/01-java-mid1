9db542d99f9834f83ffdd02b0c63d318adf4ce9c
장연식 <sseregit@knowwheresoft.com>
2024-11-27 13:53:05 +0900

    try-with-resources

--- src/exception/ex4/MainV4.java
+++ src/exception/ex4/MainV4.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class MainV4 {
 
     public static void main(String[] args) {
-        NetworkServiceV4 networkService = new NetworkServiceV4();
+//        NetworkServiceV4 networkService = new NetworkServiceV4();
+        NetworkServiceV5 networkService = new NetworkServiceV5();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -34,7 +35,7 @@ public class MainV4 {
     private static void exceptionHandler(Exception e) {
         System.out.println("사용자 메시지: 죄송합니다. 알 수 없는 문제가 발생했습니다.");
         System.out.println("==개발자용 디버깅 메시지==");
-        e.printStackTrace(System.err);
+        e.printStackTrace(System.out);
 
         if (e instanceof SendExceptionV4 sendEx) {
             System.out.println("[전송 오류] 전송 데이터: " + sendEx.getSendData());
